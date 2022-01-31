e600d9332cfa5bb2407ede56b226bd92dfd321a7
Hunter Cote <huntercote2@gmail.com>
2022-01-31 15:15:45 -0500

    updated magic-sdk and rpc url

--- src/magic.js
+++ src/magic.js
@@ -1,23 +1,29 @@
 import { Magic } from 'magic-sdk';
 import Web3 from 'web3';
 
-/** 
+/**
  * NOTE: when connecting to a testnet, TEST API keys must be used from the Magic dashboard (live API keys for eth mainnet)
  */
 
 const customNodeOptions = {
-  rpcUrl: 'https://rpc-mumbai.matic.today',
-  chainId: 80001
-}
+  rpcUrl: 'https://rpc-mumbai.maticvigil.com/',
+  chainId: 80001,
+};
 
 // Setting network to Matic
-export const magicMatic = new Magic(process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY, { network: customNodeOptions });
-magicMatic.network = "matic"
+export const magicMatic = new Magic(
+  process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY,
+  { network: customNodeOptions }
+);
+magicMatic.network = 'matic';
 
 export const maticWeb3 = new Web3(magicMatic.rpcProvider);
 
 // Setting network to Ethereum (Ropsten Testnet)
-export const magicEthereum = new Magic(process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY, { network: 'ropsten' });
-magicEthereum.network = "ethereum"
+export const magicEthereum = new Magic(
+  process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY,
+  { network: 'ropsten' }
+);
+magicEthereum.network = 'ethereum';
 
 export const ethWeb3 = new Web3(magicEthereum.rpcProvider);
