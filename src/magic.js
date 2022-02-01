fecc509bfdff468618baa3223b053a4acfc0a3ee
Hunter Cote <huntercote2@gmail.com>
2022-02-01 12:21:37 -0500

    use custom rpc for ropsten connection

--- src/magic.js
+++ src/magic.js
@@ -2,27 +2,33 @@ import { Magic } from 'magic-sdk';
 import Web3 from 'web3';
 
 /**
- * NOTE: when connecting to a testnet, TEST API keys must be used from the Magic dashboard (live API keys for eth mainnet)
+ * Configure Polygon Connection
  */
-
-const customNodeOptions = {
+const polygonNodeOptions = {
   rpcUrl: 'https://rpc-mumbai.maticvigil.com/',
   chainId: 80001,
 };
 
-// Setting network to Matic
 export const magicMatic = new Magic(
   process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY,
-  { network: customNodeOptions }
+  { network: polygonNodeOptions }
 );
 magicMatic.network = 'matic';
 
 export const maticWeb3 = new Web3(magicMatic.rpcProvider);
 
-// Setting network to Ethereum (Ropsten Testnet)
+/**
+ * Configure Ropsten Connection
+ */
+
+const ropstenNodeOptions = {
+  rpcUrl: process.env.REACT_APP_ROPSTEN_RPC,
+  chainId: 3,
+};
+
 export const magicEthereum = new Magic(
   process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY,
-  { network: 'ropsten' }
+  { network: ropstenNodeOptions }
 );
 magicEthereum.network = 'ethereum';
 
